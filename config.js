3bdb0b4eabd025b396b3ba8ae65bbc21582f4a4b
hpihn <129982700+hpihn@users.noreply.github.com>
2023-04-06 23:27:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     titleWeb: "hpihn",
     introTitle: 'Yến Nhi',
-    introDesc: `Ngay khi bạn nhìn thấy dòng chữ này, Vũ trụ đang nói với tớ rằng hãy nhắc công chúa nhỏ đặt điện thoại xuống, tắt đèn và đi ngủ đi.`,
+    introDesc: `Ngay khi em nhìn thấy dòng chữ này, Vũ trụ đang nói với anh rằng hãy nhắc công chúa nhỏ đặt điện thoại xuống, tắt đèn và đi ngủ đi.`,
     btnIntro: 'Tiếp nào',
     title: 'cô bé, em đã có một ngày dài và mệt mỏi, đã đến lúc hãy tạm quên mọi chuyện mà đi nghỉ ngơi đi nhen🌙',
     desc: 'Nếu đọc được đến dòng này thì đi ngủ đi, hết văn chương để viết ời :))',
